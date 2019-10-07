6aa93ddabe60fe27b2feac4a4cde3a0055da25ac
David Bedford <beds2004@hotmail.com>
2019-10-07 22:39:13 +0100

    Add catch all route

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,8 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/shopping-cart', [ShoppingCartController::class, 'index']);
 Route::post('/shopping-cart/{product}', [ShoppingCartController::class, 'store']);
 Route::delete('/shopping-cart/{product}', [ShoppingCartController::class, 'destroy']);
+
+/** Redirect any un-handled requests back to the products page */
+Route::get('/{any}', function () {
+    return redirect('/products');
+})->where('any', '.*');
